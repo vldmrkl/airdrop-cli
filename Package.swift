7f4459f6aa66f0586e556ff38e00f12aa6c2c1ab
Volodymyr <klymenkodp@gmail.com>
2020-12-31 00:06:15 -0500

    remove comments

--- Package.swift
+++ Package.swift
@@ -10,8 +10,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "airdrop",
             dependencies: []),
